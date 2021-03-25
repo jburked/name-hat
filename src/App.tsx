b1694ba6cbc5243860161e5816276abc866de394
Jospeh Burkiett <tjtb1@unigroup.com>
2021-03-25 11:30:04 -0500

    finally getting it and pulling it together

--- src/App.tsx
+++ src/App.tsx
@@ -1,82 +1,95 @@
-import { useState } from "react";
-import { useForm } from "react-hook-form";
+import React, { Component } from "react";
+
 import hat from "./upsideDownHat.png";
 import "./App.css";
 import "./Form.css";
-let listOfNames: string[] = [];
 
 function App() {
   return <Form />;
 }
 
-function Form() {
-  const { register, handleSubmit, errors } = useForm();
-  var initialList: string[] = [];
+class Form extends Component {
+  state: { nameList?: string[]; name?: string } = { nameList: [], name: "" };
+  // const initialList: string[] = [];
+  names = this.state.nameList;
 
-  const onSubmit = (data: any) => {
-    addNameToList(String(data.name), listOfNames);
-    console.log(data);
-  };
-  const onClick = () => {
-    listOfNames = [];
-    console.log("No name in the hate : ", listOfNames);
+  onSubmit = (n: string) => {
+    this.setState({ name: n });
   };
 
-  const onDraw = () => {
-    if (listOfNames.length > 1) {
-      alert(listOfNames[Math.floor(Math.random() * listOfNames.length)]);
+  onClear = () => {
+    this.setState({ names: [] });
+  };
+
+  onDraw = () => {
+    if (this.names && this.names.length > 1) {
+      alert(this.names[Math.floor(Math.random() * this.names.length)]);
     } else {
       alert("We need some names in the hat first!");
     }
   };
 
-  const nameList = () => {
-    const listNames = initialList.map((theName) => <li>{theName}</li>);
-    return <ul>{listNames}</ul>;
-  };
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Let's put some names in this hat!</h1>
-        <div>
-          {nameList}
+  // const nameList = () => {
+  //   const listNames = initialList.map((theName) => <li>{theName}</li>);
+  //   return <ul>{listNames}</ul>;
+  // };
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1>Let's put some names in this hat!</h1>
+          <div>
+            {this.names}
 
-          <div className="container">
-            <form onSubmit={handleSubmit(onSubmit)}>
-              <div className="row">
-                <div className="col-75">
-                  <input
-                    type="text"
-                    name="name"
-                    placeholder="Put that name in this hat"
-                    ref={register}
-                  />
-                  <input type="submit"></input>
-                  {errors.exampleRequired && <p>This field is required</p>}
-                  <button type="reset" onClick={onClick}>
-                    CLEAR
-                  </button>
+            <div className="container">
+              <form
+                onSubmit={(event: React.SyntheticEvent) => {
+                  const target = event.target as typeof event.target & {
+                    name: { value: string };
+                  };
+                  const name = target.name.value;
+                  this.onSubmit(name);
+                }}
+              >
+                <div className="row">
+                  <div className="col-75">
+                    <input
+                      type="name"
+                      name="name"
+                      placeholder="Put that name in this hat"
+                      value={this.state.name}
+                      onSubmit={(
+                        e: React.FormEvent<HTMLInputElement>
+                      ): void => {
+                        this.setState({ name: e.currentTarget.value });
+                      }}
+                    />
+                    <input type="submit"></input>
+
+                    <button type="reset" onClick={this.onClear}>
+                      CLEAR
+                    </button>
+                  </div>
                 </div>
-              </div>
-            </form>
+              </form>
+            </div>
           </div>
-        </div>
-        <div className="Hat-box">
-          <div className="Hat-box-inner">
-            {<img src={hat} className="Hat" alt="logo" onClick={onDraw} />}
+          <div className="Hat-box">
+            <div className="Hat-box-inner">
+              {
+                <img
+                  src={hat}
+                  className="Hat"
+                  alt="logo"
+                  onClick={this.onDraw}
+                />
+              }
+            </div>
           </div>
-        </div>
-      </header>
-    </div>
-  );
-}
-
-function addNameToList(name: string, listOfNames: string[]) {
-  console.log("Here is the name you added : ", name);
-
-  listOfNames.push(name);
-
-  console.log("Here is the list you added : ", listOfNames);
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
