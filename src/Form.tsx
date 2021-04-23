6d69c53e73c30c535dc0f85a5fbaa1611fc1c204
Jospeh Burkiett <tjtb1@unigroup.com>
2021-04-23 11:19:13 -0500

    adding help button for tips

--- src/Form.tsx
+++ src/Form.tsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import Cookie from "universal-cookie";
 import hat from "./upsideDownHat.png";
 import "./Form.css";
+import { makeStyles } from "@material-ui/core/styles";
 
 import Button from "@material-ui/core/Button";
 import Dialog from "@material-ui/core/Dialog";
@@ -9,10 +10,24 @@ import DialogActions from "@material-ui/core/DialogActions";
 import DialogContent from "@material-ui/core/DialogContent";
 import DialogContentText from "@material-ui/core/DialogContentText";
 import DialogTitle from "@material-ui/core/DialogTitle";
+import Fab from "@material-ui/core/Fab";
+import HelpIcon from "@material-ui/icons/Help";
 
 const style = { justifyContent: "center" };
 
+const useStyles = makeStyles((theme) => ({
+  root: {
+    "& > *": {
+      margin: theme.spacing(1),
+    },
+  },
+  extendedIcon: {
+    marginRight: theme.spacing(1),
+  },
+}));
+
 const Form = () => {
+  const classes = useStyles();
   const [name, setName] = useState("");
   const [chosenOne, setChosenOne] = useState("");
   const [list, setList] = useState<string[]>([]);
@@ -201,6 +216,15 @@ const Form = () => {
             </Button>
           </DialogActions>
         </Dialog>
+      </div>
+      <div className={classes.root}>
+        <Fab
+          color="primary"
+          aria-label="add"
+          onClick={() => setOpenThree(true)}
+        >
+          <HelpIcon />
+        </Fab>
       </div>
     </form>
   );
